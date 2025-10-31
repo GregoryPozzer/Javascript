ff0b45d2eed97873cfae29d34fa97267b3451fd3
Gregory Pozzer <pozzergregory@gmail.com>
2025-10-31 10:54:03 -0300

    Cadastro de Produtos

--- nodejs/saudacao-mvc/controllers/produtosController.js
+++ nodejs/saudacao-mvc/controllers/produtosController.js
@@ -1,4 +1,6 @@
 
+const produtosModel = require('../models/produtosModel'); 
+
 module.exports = {
 
         produtos: (req, res) => {
@@ -7,7 +9,22 @@ module.exports = {
          
         camisetas: (req, res) => {
             res.sendFile('camisetas.html', { root: './views' });
-        }                                                           
+        },     
+        
+     // Formulário de cadastro
+    formCadastrar: (req, res) => {
+        res.sendFile('formCadastrar.html', { root: './views'});
+        },
 
-    };
+    // Ação de cadastrar produto
+    cadastrar: (req, res) => {
+        const { id, descricao, quantidade, preco } = req.body;
+        const mensagemCadastro = produtosModel.cadastrar(id, descricao, quantidade, preco);
+        res.send (`<h1>${mensagemCadastro}<h1>` );
+
+    }
+
+};
+
+  
 
--- nodejs/saudacao-mvc/routes/produtos.js
+++ nodejs/saudacao-mvc/routes/produtos.js
@@ -7,6 +7,9 @@ router.get('/', produtosController.produtos);
 
 // Rota para camisetas
 router.get('/camisetas', produtosController.camisetas);
+router.get("/cadastrar", produtosController.formCadastrar);
+router.post("/cadastrar", produtosController.cadastrar);
+
 
 module.exports = router;
   
